a2b1fb422b86028daa51c298108d4ccccebe9d42
waitakere <no_springs@hotmail.com>
2026-07-30 11:10:16 +1200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    chunkSizeWarningLimit: 1500, // Supresses Phaser/Three.js size warnings
+  }
+});
